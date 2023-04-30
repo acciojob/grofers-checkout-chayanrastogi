b7dc0de1357738b508802467fbeeed7ef493d536
Chayan Rastogi <80814459+chayanrastogi@users.noreply.github.com>
2023-04-30 14:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
-
-const getSum = () => {
-//Add your code here
-  
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+const totalButton = document.querySelector('#total');
+      totalButton.addEventListener('click', () => {
+        // Get all prices from the table
+        const prices = document.querySelectorAll('.price');
+        
+        // Calculate the total price
+        let totalPrice = 0;
+        prices.forEach(price => {
+          totalPrice += parseInt(price.textContent);
+        });
+        
+        // Create a new row for the total price
+        const table = document.querySelector('table');
+        const newRow = table.insertRow();
+        const newCell= newRow.insertCell();
+        newCell.setAttribute('colspan', '2');
+        newCell.textContent = `${totalPrice}`;
+      });
